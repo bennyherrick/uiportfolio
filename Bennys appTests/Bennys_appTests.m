92553a0a196ba6e9255c95aa35832359bbf74f67
Benny Herrick <bennyherrick@gmail.com>
2019-10-17 14:04:20 -0700

    calc

---
+++ Bennys appTests/Bennys_appTests.m
@@ -0,0 +1,37 @@
+//
+//  Bennys_appTests.m
+//  Bennys appTests
+//
+//  Created by Herrick, Benjamin on 10/10/19.
+//  Copyright © 2019 Herrick, Benjamin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bennys_appTests : XCTestCase
+
+@end
+
+@implementation Bennys_appTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
