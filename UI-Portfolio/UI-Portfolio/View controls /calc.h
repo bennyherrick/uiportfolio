f7003bb66401b740a1d81637bf289f6769bccfd6
Benny Herrick <bennyherrick@gmail.com>
2019-11-07 13:57:40 -0800

    commit calculator

---
+++ UI-Portfolio/UI-Portfolio/View controls /calc.h
@@ -0,0 +1,13 @@
+//
+//  calc.h
+//  UI-Portfolio
+//
+//  Created by Herrick, Benjamin on 11/7/19.
+//  Copyright © 2019 Herrick, Benjamin. All rights reserved.
+//
+
+#ifndef calc_h
+#define calc_h
+
+
+#endif /* calc_h */
---
+++ UI-Portfolio/UI-Portfolio/View controls /calc.m
@@ -0,0 +1,9 @@
+//
+//  calc.m
+//  UI-Portfolio
+//
+//  Created by Herrick, Benjamin on 11/7/19.
+//  Copyright © 2019 Herrick, Benjamin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
